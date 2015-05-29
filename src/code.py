4241800fae708704cf90574dd51e462a5543b62c
Eric Talevich <eric.talevich@gmail.com>
2015-05-29 13:56:31 -0700

    CNVkit 0.5.0, pysam 0.8.3

--- src/code.py
+++ src/code.py
@@ -40,7 +40,7 @@ def main(tumor_bams=None, normal_bams=None, cn_reference=None,
                                           "3.0"])
     # Install CNVkit itself
     sh("pip install -v --no-index --find-links=file:///requirements -r requirements.txt")
-    sh("pip install -v --no-index --no-deps /requirements/CNVkit-0.3.5.tar.gz")
+    sh("pip install -v --no-index --no-deps /requirements/CNVkit-0.5.0.tar.gz")
 
     print("Downloading file inputs to the local file system")
     cn_reference = download_link(cn_reference)
