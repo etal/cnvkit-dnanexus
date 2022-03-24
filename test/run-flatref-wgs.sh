fe200ba0db80257330f6d4bef71397f4d742a807
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-25 02:54:53 +0800

    Delete test directory

--- test/run-flatref-wgs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Build a WGS flat reference (no samples, just the reference genome)
-set -ex
-dx select project-FFFkxQ00vzzYqffyGg9XyQXQ
-dx run cnvkit_batch --watch -y \
-    -imethod=wgs \
-    -ifasta=file-B6ZY7VG2J35Vfvpkj8y0KZ01 \
-    --name="CNVkit $0" \
-    --folder=tmp/
